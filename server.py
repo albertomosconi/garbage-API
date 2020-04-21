10014ef157ad83b9adacdb05cd0d10ee1275d1b5
Alberto Mosconi <pitagora99@gmail.com>
2020-04-21 17:25:27 +0200

    cleaned code

--- server.py
+++ server.py
@@ -1,26 +1,15 @@
-# from flask import Flask, request
-# app = Flask(__name__)
-
-
-# @app.route('/', methods=['GET', 'POST'])
-# def hello_world():
-#     if request.method == 'GET':
-#         return "hello world!"
-#     elif request.method == 'POST':
-#         return request.files
-import os
-from flask import Flask, flash, request, jsonify, redirect, url_for, Response
-from werkzeug.utils import secure_filename
-
-from PIL import Image
+import base64
 import json
+import os
 
-import torch
-import torch.nn as nn
-from torchvision import models, transforms
 import cv2
 import numpy as np
-import base64
+import torch
+import torch.nn as nn
+from flask import Flask, Response, flash, jsonify, redirect, request, url_for
+from PIL import Image
+from torchvision import models, transforms
+from werkzeug.utils import secure_filename
 
 data_transforms = {
     'val': transforms.Compose([
@@ -43,18 +32,7 @@ def image_loader(loader, image_name):
     return image
 
 
-UPLOAD_FOLDER = 'prova/'
-ALLOWED_EXTENSIONS = {"jpg", "png", "jpeg"}
-
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-app.secret_key = b'_5#y2L"F4Q8z\n\xec]/'
-
-
-def allowed_file(filename):
-    return '.' in filename and \
-           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -63,26 +41,22 @@ def upload_file():
         r = request
         image = r.files.get("photo", '')
         content = image.read()
-        # convert string of image data to uint8
-        # print(data['image'])
-        # d = base64.decodebytes(data['image'])
+
         with open("aaa.jpg", "wb") as f:
             f.write(content)
-        # print(str(content))
+
         nparr = np.frombuffer(content, dtype=np.uint8)
         print(nparr, len(nparr))
+
         # decode image
         img = cv2.imdecode(nparr, cv2.IMREAD_COLOR)
 
         width = len(img[0])
         cropped_image = img[int(width/2):-int(width/2)]
-        # cv2.imshow('image', cropped_image)
-        # cv2.waitKey(0)
 
         model = models.resnet18(pretrained=True)
         num_ftrs = model.fc.in_features
-        # Here the size of each output sample is set to 2.
-        # Alternatively, it can be generalized to nn.Linear(num_ftrs, len(class_names)).
+
         model.fc = nn.Linear(num_ftrs, 6)
         model.load_state_dict(torch.load("model.pt", map_location='cpu'))
         model.eval()
@@ -106,10 +80,7 @@ def upload_file():
             predicted_class = 'trash'
 
         return jsonify('{"result":"' + predicted_class + '"}')
-    elif request.method == 'GET':
-        response = '{"name": "Brian", "city": "Seattle"}'
-        return jsonify(response)
 
 
 if __name__ == "__main__":
-    app.run(host="192.168.0.12", port=5010)
+    app.run(host="192.168.0.9", port=5010)
